59210eecb547bee582f2a347914bb4ba49eee521
youjae <youjae0803@gmail.com>
2024-01-31 00:27:41 +0900

    fix: host 수정

--- src/main/java/com/example/webcompiler/WebSocketApplication.java
+++ src/main/java/com/example/webcompiler/WebSocketApplication.java
@@ -8,6 +8,7 @@ import com.github.dockerjava.core.DockerClientConfig;
 import com.github.dockerjava.jaxrs.JerseyDockerCmdExecFactory;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +16,8 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class WebSocketApplication {
 
+	@Value("${ec2.info.host}")
+	private String host;
 	public static void main(String[] args) {
 		SpringApplication.run(WebSocketApplication.class, args);
 	}
@@ -31,7 +34,7 @@ public class WebSocketApplication {
 		DockerCmdExecFactory dockerCmdExecFactory = new JerseyDockerCmdExecFactory();
 
 		DockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder()
-				.withDockerHost("tcp://52.79.194.35:2375")
+				.withDockerHost("tcp://"+host+":2375")
 				.build();
 
 		DockerClient dockerClient = DockerClientBuilder.getInstance(config).build();
