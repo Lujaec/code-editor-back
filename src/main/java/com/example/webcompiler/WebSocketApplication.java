aee0a50c56489012edfb93558146ca165d0eeee1
youjae <youjae0803@gmail.com>
2024-02-01 11:35:02 +0900

    fix: cors 문제 해결

--- src/main/java/com/example/webcompiler/WebSocketApplication.java
+++ src/main/java/com/example/webcompiler/WebSocketApplication.java
@@ -16,6 +16,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Properties;
 
@@ -68,5 +70,16 @@ public class WebSocketApplication {
 
 		return session;
 	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
+
 
 }
